3265bb6b332cb5d50c2150dca7ef5504363f808b
BreezeBM <seyya77@gmail.com>
2021-01-14 20:37:16 +0900

    [ADD] routes first setting compelete

---
+++ routes/posts.ts
@@ -0,0 +1,16 @@
+const express = require("express");
+const postsRouter = express.Router();
+
+const { postsController } = require("../controller");
+
+// 모든 포스트를 가져온다
+postsRouter.get("/", postsController.getPostsAll.get);
+
+//1개의 게시글(post)를 작성하여 데이터베이스에 넣는다.
+// id는 user_id를 의미한다.
+postsRouter.post("/:id", postsController.writingPost.post);
+
+// 1개의 post를 닫는다. 여기서 id는 post_id를 의미한다.
+postsRouter.put("/:id", postsController.closingPost.put);
+
+export default postsRouter;
---
+++ routes/search.ts
@@ -0,0 +1,8 @@
+const express = require("express");
+const searchRouter = express.Router();
+const { searchController } = require("../controller");
+
+//그룹 현황을 조회(검색)한다.
+searchRouter.get("/", searchController.searchGroup.get);
+
+export default searchRouter;
