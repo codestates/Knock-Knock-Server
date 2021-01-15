7f469b369b4eb2495147eac88fa459aaa2bb6303
Vesuvio <68624696+yrkimyy@users.noreply.github.com>
2021-01-15 13:45:03 +0900

    [MODIFY] feature/#5/Controller twisted gitflow

--- routes/search.ts
+++ routes/search.ts
@@ -1,8 +1,8 @@
-const express = require("express");
-const searchRouter = express.Router();
-const { searchController } = require("../controller");
+// const express = require("express");
+// const searchRouter = express.Router();
+// const { searchController } = require("../controller");
 
-//그룹 현황을 조회(검색)한다.
-searchRouter.get("/", searchController.searchGroup.get);
+// //그룹 현황을 조회(검색)한다.
+// searchRouter.get("/", searchController.searchGroup.get);
 
-export default searchRouter;
+// export default searchRouter;
---
+++ src/entity/pstack.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany } from "typeorm";
+import { Post } from "./Post";
+
+@Entity()
+export class Pstack {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    stackName: string;
+
+    @ManyToOne(() => Post, post => post.id)
+    post: Post;
+
+}
