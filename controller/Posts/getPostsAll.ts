c5b60e68f861babb363925340c66e6e536b8619d
BreezeBM <seyya77@gmail.com>
2021-01-17 21:47:42 +0900

    [ADD] diary and join controller first done

--- controller/Posts/getPostsAll.ts
+++ controller/Posts/getPostsAll.ts
@@ -2,15 +2,12 @@ import { Request, Response, NextFunction } from "express";
 import { User } from "../../src/entity/User";
 import { Post } from "../../src/entity/Post";
 
-
 export default async (req: Request, res: Response): Promise<void> => {
-    const results = await Post.allPost();
-    
-    if (results) {
-        res.status(200).send("ok")
-        console.log(results)
-    }
-    else {
-        res.status(404).send({"message" : ":p"})
-    }
-}
+  const postResult = await Post.allPost();
+
+  if (postResult) {
+    res.status(200).send({ data: postResult });
+  } else {
+    res.status(404).send({ message: "post not found" });
+  }
+};
