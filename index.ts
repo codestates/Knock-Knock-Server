7267a459a408d5cb3b50f9c787cc0854597361e9
Vesuvio <68624696+yrkimyy@users.noreply.github.com>
2021-01-27 11:38:44 +0900

    [MODIFY] modified index.ts for pm2 and ubuntu deploy setting

--- index.ts
+++ index.ts
@@ -9,14 +9,14 @@ import * as dotenv from "dotenv";
 import * as routes from "./src/routes";
 import * as bodyParser from "body-parser";
 
+const nodeSchedule = require("node-schedule");
 dotenv.config();
 
-const fs = require("fs");
-const https = require("https");
-const nodeSchedule = require("node-schedule");
-const privateKey = fs.readFileSync("./key.pem", "utf8");
-const certificate = fs.readFileSync("./cert.pem", "utf8");
-const credentials = { key: privateKey, cert: certificate };
+// const fs = require("fs");
+// const https = require("https");
+// const privateKey = fs.readFileSync("./key.pem", "utf8");
+// const certificate = fs.readFileSync("./cert.pem", "utf8");
+// const credentials = { key: privateKey, cert: certificate };
 
 const port = 4000;
 const app = express();
@@ -98,12 +98,12 @@ app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello World")
 })
 
-const httpsServer = https.createServer(credentials, app);
-httpsServer.listen(port);
-module.exports = httpsServer;
+// const httpsServer = https.createServer(credentials, app);
+// httpsServer.listen(port);
+// module.exports = httpsServer;
 
-// app.listen(port, () => {
-//   console.log('done')
-// });
+app.listen(port, () => {
+  console.log('done')
+});
 
-// module.exports = app;
+module.exports = app;
